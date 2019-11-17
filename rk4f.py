8b2c5ec2a379df7f0c903a4e0601e10da2163936
Default user <n.bahamonn@uniandes.edu.co>
2019-11-17 22:31:11 +0000

    Grafica rk4 con friccion

--- rk4f.py
+++ rk4f.py
@@ -5,9 +5,16 @@ data = np.loadtxt('rk4f.dat')
 
 plt.figure(figsize=(14,10))
 
+plt.subplot(2,1,1) 
 plt.title("rk4 fric = 0.4")
 plt.xlabel("t")
 plt.ylabel("y0")
 plt.plot(data[:,0],data[:,1])
+
+plt.subplot(2,1,2) 
+plt.title("Comparacion y0, y1")
+plt.xlabel("y0")
+plt.ylabel("y1")
+plt.plot(data[:,1],data[:,2])
 
 plt.savefig("rk4fric.png")
